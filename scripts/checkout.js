ce5c679fdcb673980ce729fc22fe72c576119a2b
shivam <shivamtiwarisgbt@gmail.com>
2026-05-05 14:55:24 +0530

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import '../data/cart-oop.js'
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymentSummary();
--- scripts/checkout/ordersummary.js
+++ scripts/checkout/ordersummary.js
@@ -31,6 +31,7 @@ import { renderCheckoutHeader } from './checkoutHeader.js';
     
     cartSummaryHTML += `
       <div class="cart-item-container
+      js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: ${dateString}
@@ -47,14 +48,14 @@ import { renderCheckoutHeader } from './checkoutHeader.js';
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
               </span>
               <span class="update-quantity-link link-primary">
                 Update
               </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
