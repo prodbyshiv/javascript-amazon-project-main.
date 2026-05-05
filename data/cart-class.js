d95148f4042f58613ca8f946fc3dada166f0904f
shivam <shivamtiwarisgbt@gmail.com>
2026-05-05 15:20:00 +0530

    Mkae LocalStoargeKey and LoadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,16 +1,16 @@
 class Cart {
-    cartItems;
-    localStorageKey;
+    cartItems; // public property can  be accessed outside class
+    #localStorageKey; // private property: it can not be accessed
 
 
     constructor(localStorageKey){
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
         
     }
 
-    loadFromStorage() {
-  this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage() {
+  this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
 
 
   if (!this.cartItems) {
@@ -26,7 +26,7 @@ class Cart {
 }
 }
      saveToStorage() {
-  localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
 }
 
     addToCart(productId) {
